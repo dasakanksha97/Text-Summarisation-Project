5e708220b40a1e24418dd51a0fba17086aa4a525
Akanksha Das <adas@flyanra.com>
2023-09-17 10:54:31 -0400

    Data Ingestion Notebook added

---
+++ main.py
@@ -0,0 +1,16 @@
+# from src.textSummarizer.logging import logger
+
+# logger.info("Welcome to custom logging system")
+
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.logging import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<<<\n\n_____________")
+except Exception as e:
+    logger.exception(e)
+    raise e
